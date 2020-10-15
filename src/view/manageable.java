68fc2c23979e1f14a29cf57daf6393e90f322cb6
shacharml <68264925+shacharml@users.noreply.github.com>
2020-10-15 09:20:08 +0300

    Update manageable.java

--- src/view/manageable.java
+++ src/view/manageable.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import MyExceptions.AnswerDontExists;
 import MyExceptions.MoreQuestionsThenExist;
 import listeners.SystemUIEventListener;
-import model.AdminSystem;
+
 
 public interface manageable {
 
@@ -24,15 +24,11 @@ public interface manageable {
 
 	void deleteAnswer();
 
-	void createAnExamManualy(AdminSystem adminSystem) throws FileNotFoundException, MoreQuestionsThenExist, AnswerDontExists;
+	void createAnExamManualy() throws FileNotFoundException, MoreQuestionsThenExist, AnswerDontExists;
 
 	void creatAnExamAutomatic(String autoExam) throws MoreQuestionsThenExist, FileNotFoundException;
 
 	void registerListener(SystemUIEventListener manegmentSystemController);
 
 	//void createAnExamManualy(AdminSystem adminSystem);
-
-
-	
-
 }
